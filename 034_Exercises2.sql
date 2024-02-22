add53373a8989fb969dac5bbd9dcea2dc61a6c49
kekonline <kekonline@gmail.com>
2024-02-22 20:27:51 +0100

    more sql code

---
+++ 034_Exercises2.sql
@@ -0,0 +1,39 @@
+
+-- Create the following tables in MySQL
+
+-- Products
+
+-- primary key
+-- name
+-- category
+-- sell by date
+-- sold or not
+-- moment of sale
+-- quantity
+-- weight Kg
+
+-- ---------------------------------------
+-- Personnel
+
+-- primary key
+-- given name
+-- family name
+-- gender
+-- telephone number
+-- marital status
+-- age
+-- salary
+-- position
+-- date started
+
+-- ---------------------------------------
+
+-- Address
+
+-- First line
+-- Second line
+-- City
+-- Region
+-- zip/postal code
+-- Two letter country code
+
