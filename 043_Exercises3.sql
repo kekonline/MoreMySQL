a7c0f61a45746c38b431fcac57a97f56c2a90a11
kekonline <kekonline@gmail.com>
2024-02-23 18:29:44 +0100

    joins and basic examples

---
+++ 043_Exercises3.sql
@@ -0,0 +1,28 @@
+-- Answer using only one SQL query for each question.
+
+--
+-- 1. Find out the average weight for each country.
+--
+
+--
+-- 2. Create a list of the number of respondents from each country. Order the list by the number of respondents. 
+--     Show only those countries where the number of respondents was greater than 3.
+--
+
+--
+-- 3. Display the average height for each country. Show also the number of respondents for each country and order the list by average height.
+-- 
+
+
+--
+-- 4. For each country, find the average weight of both men and women in that country, and the number of respondents in each gender-country category. 
+-- Display only those categories containing more than two respondents. Order the results by country.
+-- 
+
+
+-- 
+-- 5. For each of the possible four answers to the exercise question, display the average health score for the respondents in that group. Order from poor health to good health.
+-- Is there any relationship between reported amount of exercise and reported state of health? If so, why?
+--
+
+
